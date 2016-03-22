99d33639011cbb2f9bf0fa349943be8510b3983e
gyanesh <pgyanesh.patra@gmail.com>
2016-03-22 16:54:59 -0300

    CleanUp

--- test_pcap1.sh
+++
@@ -1,8 +0,0 @@
-
-ARCH=pcap
-
-rm -rf build
-
-make -f Makefile_pcap.mk && \
-(cd build; mkdir -p output; ./example_pcap1 ../examples/table_config_$ARCH.cfg ../examples/captures/ipv4_cipso_option.pcap 1000 1001 1002) && \
-echo "Test execution finished."
