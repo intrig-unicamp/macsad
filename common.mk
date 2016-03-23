90eb1b9a9c19c785f5aacc8dc0cd2404681cbeb9
c3m3gyanesh <pgyanesh.patra@gmail.com>
2016-03-23 22:55:59 +0000

    cleanup for Make

--- common.mk
+++ common.mk
@@ -1,16 +1,6 @@
-
-ifeq ($(RTE_SDK),)
-$(error "Please define RTE_SDK environment variable")
-endif
-
 ifeq ($(ODP_SDK),)
 $(error "Please define ODP_SDK environment variable")
 endif
-
-# Default target, can be overriden by command line or environment
-#RTE_TARGET ?= x86_64-native-linuxapp-gcc
-
-#include $(RTE_SDK)/mk/rte.vars.mk
 
 # Compiler debug level
 CFLAGS += -O3
@@ -23,8 +13,7 @@ CFLAGS += -Wno-unused-value
 
 CFLAGS += -std=gnu99
 
-#To define the backend used: odp or dpdk
+#To define the backend used: odp/dpdk
 CFLAGS += -D ODP_BK
-#CFLAGS += -D ODP_BK
 
 #CFLAGS += -always-make
--- odp_backend.mk
+++ odp_backend.mk
@@ -5,47 +5,46 @@ VPATH += $(CDIR)/src/hardware_dep/odp/includes
 VPATH += $(CDIR)/src/hardware_dep/odp/ctrl_plane
 VPATH += $(CDIR)/src/hardware_dep/odp/data_plane
 
-VPATH += $(ODP_SDK)include
-VPATH += $(ODP_SDK)platform/linux-generic/include
-VPATH += $(ODP_SDK)platform/linux-generic/arch/x86
-VPATH += $(ODP_SDK)share
-VPATH += $(ODP_SDK)helper/include
-VPATH += $(ODP_SDK)helper/include/odp
+VPATH += $(ODP_SDK)/include
+VPATH += $(ODP_SDK)/platform/linux-generic/include
+VPATH += $(ODP_SDK)/platform/linux-generic/arch/x86
+VPATH += $(ODP_SDK)/share
+VPATH += $(ODP_SDK)/helper/include
+VPATH += $(ODP_SDK)/helper/include/odp
 
 # odp main
-SRCS-y += $(CDIR)src/hardware_dep/odp/main.c
-SRCS-y += $(CDIR)src/hardware_dep/odp/main_odp.c
+SRCS-y += $(CDIR)/src/hardware_dep/odp/main.c
+SRCS-y += $(CDIR)/src/hardware_dep/odp/main_odp.c
 
 # control plane related sources
-SRCS-y += $(CDIR)src/hardware_dep/odp/ctrl_plane/ctrl_plane_backend.c
-SRCS-y += $(CDIR)src/hardware_dep/odp/ctrl_plane/fifo.c
-SRCS-y += $(CDIR)src/hardware_dep/odp/ctrl_plane/handlers.c
-SRCS-y += $(CDIR)src/hardware_dep/odp/ctrl_plane/messages.c
-SRCS-y += $(CDIR)src/hardware_dep/odp/ctrl_plane/sock_helpers.c
-SRCS-y += $(CDIR)src/hardware_dep/odp/ctrl_plane/threadpool.c
+SRCS-y += $(CDIR)/src/hardware_dep/odp/ctrl_plane/ctrl_plane_backend.c
+SRCS-y += $(CDIR)/src/hardware_dep/odp/ctrl_plane/fifo.c
+SRCS-y += $(CDIR)/src/hardware_dep/odp/ctrl_plane/handlers.c
+SRCS-y += $(CDIR)/src/hardware_dep/odp/ctrl_plane/messages.c
+SRCS-y += $(CDIR)/src/hardware_dep/odp/ctrl_plane/sock_helpers.c
+SRCS-y += $(CDIR)/src/hardware_dep/odp/ctrl_plane/threadpool.c
 
 # data plane related includes
-SRCS-y += $(CDIR)src/hardware_dep/odp/data_plane/odp_lib.c
-SRCS-y += $(CDIR)src/hardware_dep/odp/data_plane/odp_tables.c
-SRCS-y += $(CDIR)src/hardware_dep/odp/data_plane/odp_primitives.c
-#SRCS-y += $(CDIR)src/hardware_dep/odp/data_plane/ternary_naive.c
+SRCS-y += $(CDIR)/src/hardware_dep/odp/data_plane/odp_lib.c
+SRCS-y += $(CDIR)/src/hardware_dep/odp/data_plane/odp_tables.c
+SRCS-y += $(CDIR)/src/hardware_dep/odp/data_plane/odp_primitives.c
+#SRCS-y += $(CDIR)/src/hardware_dep/odp/data_plane/ternary_naive.c
 
-CFLAGS += -I "$(CDIR)src/hardware_dep/odp/includes"
-CFLAGS += -I "$(CDIR)src/hardware_dep/odp/ctrl_plane"
-CFLAGS += -I "$(CDIR)src/hardware_dep/odp/data_plane"
+CFLAGS += -I "$(CDIR)/src/hardware_dep/odp/includes"
+CFLAGS += -I "$(CDIR)/src/hardware_dep/odp/ctrl_plane"
+CFLAGS += -I "$(CDIR)/src/hardware_dep/odp/data_plane"
 
-CFLAGS += -I "$(ODP_SDK)include"
-CFLAGS += -I "$(ODP_SDK)platform/linux-generic/include"
-CFLAGS += -I "$(ODP_SDK)platform/linux-generic/arch/x86"
-CFLAGS += -I "$(ODP_SDK)share"
-CFLAGS += -I "$(ODP_SDK)helper/include"
-CFLAGS += -I "$(ODP_SDK)helper/include/odp"
+CFLAGS += -I "$(ODP_SDK)/include"
+CFLAGS += -I "$(ODP_SDK)/platform/linux-generic/include"
+CFLAGS += -I "$(ODP_SDK)/platform/linux-generic/arch/x86"
+CFLAGS += -I "$(ODP_SDK)/share"
+CFLAGS += -I "$(ODP_SDK)/helper/include"
+CFLAGS += -I "$(ODP_SDK)/helper/include/odp"
 
-LDFLAGS += -L $(ODP_SDK)lib/
-LDFLAGS += -L $(ODP_SDK)lib/.libs
+LDFLAGS += -L $(ODP_SDK)/lib/
+LDFLAGS += -L $(ODP_SDK)/lib/.libs
 LIBS = -lodp -lodphelper -lpthread
 
-#LIBTOOL = $(SHELL) $(ODP_SDK)libtool
 #$(info SRCS-y is $(SRCS-y))
 
 OBJS = $(SRCS-y:.c=.o)
@@ -58,13 +57,6 @@ all: $(MAIN)
 $(MAIN):
 #	$(CC) $(CFLAGS) $(SRCS-y) -o $@ $(LDFLAGS)
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $(MAIN) $(SRCS-y) $(LIBS)
-
-#$(MAIN): $(OBJS)
-#       $(CC) $(CFLAGS) odp_pktio.c -o  $@ -lodp
-#	$(CC) $(CFLAGS) main.c -o  $@ $(LDFLAGS)
-#       $(LIBTOOL) --mode=link $(CC) $(CFLAGS) odp_pktio.c -o $(LDADD) $@ $(LDFLAGS)
-#enable this
-#	$(CC) $(CFLAGS)  -o $(MAIN) $(OBJS) $(LDFLAGS)
 
 .c.o:
 	$(CC) $(CFLAGS) -c $<  -o $@
