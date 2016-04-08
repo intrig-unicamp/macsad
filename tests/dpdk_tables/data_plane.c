6274c305649fe313fbe29a654e35ab26ee1c7cd5
Gyanesh Patra <pgyanesh.patra@gmail.com>
2016-04-08 13:36:25 -0300

    removed tests

--- tests/dpdk_tables/data_plane.c
+++
@@ -1,5 +0,0 @@
-
-#include "dpdk_lib.h"
-
-void p4_handle_packet(packet *p, unsigned portid) {
-}
--- tests/dpdk_tables/header_info.h
+++
@@ -1,5 +0,0 @@
-#ifndef __HEADER_INFO_H__
-#define __HEADER_INFO_H__
-
-
-#endif // __HEADER_INFO_H__
