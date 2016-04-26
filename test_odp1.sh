42167b1865617f44720e736602fdb39f9ccaaf5c
Gyanesh Patra <c3m3gyanesh@users.noreply.github.com>
2016-04-26 17:17:27 -0300

    Update test_odp1.sh

--- test_odp1.sh
+++ test_odp1.sh
@@ -1,8 +1,5 @@
 
 # set default values for variables if they are not already defined
-#ARCH=${ARCH-odp}
-#P4DPDK_OPTS=${P4DPDK_OPTS--c 0x3 -n 2 -- -P -p 0x3 --config "(0,0,0),(1,0,0)"}
-#P4DPDK_OPTS=${P4DPDK_OPTS--c 3 -n 2 -- -P -p 0x3 --config "(0,0,0),(1,0,0)"}
 MAKE_CMD=${MAKE_CMD-make}
 
 # Compile test controller
@@ -11,27 +8,22 @@ make clean
 make mac_controller
 cd -
 
-echo "odp ctrl make done"
+echo "Controller compilation complete."
 
 # Restart test controller in background
 sudo killall mac_controller
-sudo killall mac_l3_controller
-sudo killall mac_l3-full_controller
 ./src/hardware_dep/shared/ctrl_plane/mac_controller &
 
+echo "Controller started... "
 
-echo "odp ctrl start "
-# Complete rebuild starting...
-#rm -rf build
+echo "Creating auto-generted files from p4 source...
+rm -rf build
+python src/compiler.py examples/p4_src/l2_switch_test.p4
 
-#python src/compiler.py examples/p4_src/l2_switch_test.p4
 # Compile C sources
 #${MAKE_CMD} -j16
 
-# Should the program produce any output, it will be put here
-mkdir -p build/output
-
-# Start the program
-#echo "run build/example_odp1"
-#sudo ./build/example_odp1 ${P4DPDK_OPTS}
+# Start the switch
+echo "Run mac_ad with veth1.0 and veth2.1"
+sudo ./mac_ad -i veth1.0,veth2.1
 
