ae921180c0e95d3c6dc4d1cf79962f73ca7ea6c5
Gyanesh Patra <pgyanesh.patra@gmail.com>
2016-04-27 12:19:59 -0300

    merge1

--- src/hardware_dep/shared/ctrl_plane/controller.c
+++ src/hardware_dep/shared/ctrl_plane/controller.c
@@ -25,6 +25,7 @@ typedef struct controller_st
         fifo_t output_queue;
         int controller_fd;
 	digest_handler dh;
+	initialize init;
 	int port;
 } controller_t;
 
@@ -71,6 +72,46 @@ void output_processor(void *t)
         }
 }
 
+controller create_controller_with_init(uint16_t port, int number_of_threads, digest_handler dh, initialize init)
+{
+	controller_t* ct;
+	struct sockaddr_in server;
+	/*struct sockaddr_in client;*/
+
+	if ((port<=0) || (number_of_threads<=0))
+		return 0;
+
+	ct = (controller_t*) malloc(sizeof(controller_t));
+	
+	if (ct==0) 
+		return 0;
+
+    ct->tpool = create_threadpool(3);/*number_of_threads);*/
+
+    fifo_init(&(ct->input_queue));
+    fifo_init(&(ct->output_queue));
+	ct->dh = dh;
+	ct->init = init;
+
+	ct->controller_fd = socket( PF_INET, SOCK_STREAM, 0 );
+
+	/* Server's information*/
+	server.sin_family = AF_INET;	
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(port);
+	ct->port = port;
+
+	/* binding the addressing information to the listen socket*/
+	bind( ct->controller_fd, (struct sockaddr *) &server, sizeof server );
+
+	listen( ct->controller_fd, 5 );
+	
+	return (controller)ct;
+
+}
+
+
+
 controller create_controller(uint16_t port, int number_of_threads, digest_handler dh)
 {
 	controller_t* ct;
@@ -90,6 +131,7 @@ controller create_controller(uint16_t port, int number_of_threads, digest_handle
         fifo_init(&(ct->input_queue));
         fifo_init(&(ct->output_queue));
 	ct->dh = dh;
+	ct->init = 0;
 
 	ct->controller_fd = socket( PF_INET, SOCK_STREAM, 0 );
 
@@ -148,14 +190,18 @@ void execute_controller(controller c)
 			                        break;
 
 					printf("New device connected...\n");
-			                ti = (threadinfo_t*) malloc(sizeof(threadinfo_t));
-			                ti->ct = ct;
-			                ti->sock_fd = conn;
+			        ti = (threadinfo_t*) malloc(sizeof(threadinfo_t));
+			        ti->ct = ct;
+			        ti->sock_fd = conn;
+
 					dispatch(ct->tpool, output_processor, (void*)ti);
 					sleep(1);
-				        dispatch(ct->tpool, input_processor, (void*)ti);
+				    dispatch(ct->tpool, input_processor, (void*)ti);
 					FD_SET(conn, &master);
 					maxfds = conn>maxfds?conn:maxfds;
+					printf("Initialize switch\n");
+					if (ct->init!=0)
+						ct->init();
 				} else {
 					mem_cell = (msg_buf_t*)malloc(sizeof(msg_buf_t));
 					mem_cell->length = 2048;
