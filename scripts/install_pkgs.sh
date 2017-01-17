88c44bc3ecf06b59efc570e518de889bdcea620f
Gyanesh Patra <c3m3gyanesh@users.noreply.github.com>
2017-01-17 12:07:17 -0200

    pkg list updated for ODP

--- scripts/install_pkgs.sh
+++ scripts/install_pkgs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Install packages necessary for MACSAD and its submodules
-
-sudo apt-get install -y make
-sudo apt-get install -y libtool
 sudo apt-get install -y git
+sudo apt-get install -y automake 
+sudo apt-get install -y autoconf 
+sudo apt-get install -y libtool
