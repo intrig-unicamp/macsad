ace707799450d522e9360a72ca817095535b1743
Gyanesh Patra <c3m3gyanesh@users.noreply.github.com>
2017-01-17 12:10:29 -0200

    openssl libraries

--- scripts/install_pkgs.sh
+++ scripts/install_pkgs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #Install packages necessary for MACSAD and its submodules
-sudo apt-get install -y git
 sudo apt-get install -y automake 
 sudo apt-get install -y autoconf 
 sudo apt-get install -y libtool
+sudo apt-get install -y git
+
+# Libraries for OpenSSL for ODP
+#sudo apt-get install libssl-dev
