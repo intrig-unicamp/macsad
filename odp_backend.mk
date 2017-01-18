23495677ed6ab12b1b5f019cd71f4c6331a38c0f
c3m3gyanesh <pgyanesh.patra@gmail.com>
2017-01-18 14:26:25 +0000

    path name cleanup for make

--- odp_backend.mk
+++ odp_backend.mk
@@ -1,6 +1,6 @@
 CDIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-VPATH += $(CDIR)/src/hardware_dep/odp/
+VPATH += $(CDIR)/src/hardware_dep/odp
 VPATH += $(CDIR)/src/hardware_dep/odp/includes
 VPATH += $(CDIR)/src/hardware_dep/odp/ctrl_plane
 VPATH += $(CDIR)/src/hardware_dep/odp/data_plane
@@ -11,9 +11,9 @@ VPATH += $(ODP_SDK)/platform/linux-generic/arch/x86
 VPATH += $(ODP_SDK)/share
 VPATH += $(ODP_SDK)/helper/include/odp/helper
 
-CFLAGS += -I "$(CDIR)/src/hardware_dep/odp/includes"
-CFLAGS += -I "$(CDIR)/src/hardware_dep/odp/ctrl_plane"
-CFLAGS += -I "$(CDIR)/src/hardware_dep/odp/data_plane"
+CFLAGS += -I "$(CDIR)src/hardware_dep/odp/includes"
+CFLAGS += -I "$(CDIR)src/hardware_dep/odp/ctrl_plane"
+CFLAGS += -I "$(CDIR)src/hardware_dep/odp/data_plane"
 
 #ODP APIs
 CFLAGS += -I "$(ODP_SDK)/include"
@@ -28,21 +28,21 @@ CFLAGS += -I "$(ODP_SDK)/helper/include/odp"
 CFLAGS += -I "$(ODP_SDK)/helper/include/odp/helper"
 
 # odp main
-SRCS-y += $(CDIR)/src/hardware_dep/odp/main.c
-SRCS-y += $(CDIR)/src/hardware_dep/odp/main_odp.c
+SRCS-y += $(CDIR)src/hardware_dep/odp/main.c
+SRCS-y += $(CDIR)src/hardware_dep/odp/main_odp.c
 
 # control plane related sources
-SRCS-y += $(CDIR)/src/hardware_dep/shared/ctrl_plane/ctrl_plane_backend.c
-SRCS-y += $(CDIR)/src/hardware_dep/shared/ctrl_plane/fifo.c
-SRCS-y += $(CDIR)/src/hardware_dep/shared/ctrl_plane/handlers.c
-SRCS-y += $(CDIR)/src/hardware_dep/shared/ctrl_plane/messages.c
-SRCS-y += $(CDIR)/src/hardware_dep/shared/ctrl_plane/sock_helpers.c
-SRCS-y += $(CDIR)/src/hardware_dep/shared/ctrl_plane/threadpool.c
+SRCS-y += $(CDIR)src/hardware_dep/shared/ctrl_plane/ctrl_plane_backend.c
+SRCS-y += $(CDIR)src/hardware_dep/shared/ctrl_plane/fifo.c
+SRCS-y += $(CDIR)src/hardware_dep/shared/ctrl_plane/handlers.c
+SRCS-y += $(CDIR)src/hardware_dep/shared/ctrl_plane/messages.c
+SRCS-y += $(CDIR)src/hardware_dep/shared/ctrl_plane/sock_helpers.c
+SRCS-y += $(CDIR)src/hardware_dep/shared/ctrl_plane/threadpool.c
 
 # data plane related includes
-SRCS-y += $(CDIR)/src/hardware_dep/odp/data_plane/odp_lib.c
-SRCS-y += $(CDIR)/src/hardware_dep/odp/data_plane/odp_tables.c
-SRCS-y += $(CDIR)/src/hardware_dep/odp/data_plane/odp_primitives.c
+SRCS-y += $(CDIR)src/hardware_dep/odp/data_plane/odp_lib.c
+SRCS-y += $(CDIR)src/hardware_dep/odp/data_plane/odp_tables.c
+SRCS-y += $(CDIR)src/hardware_dep/odp/data_plane/odp_primitives.c
 #SRCS-y += odp_tables.c
 #SRCS-y += odp_primitives.c
 SRCS-Y += vector.c
