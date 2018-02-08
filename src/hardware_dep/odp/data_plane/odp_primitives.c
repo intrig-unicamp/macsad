c3be949e31a2f62721b59df79313ad3f52258ab7
root <dlfeferman@gmail.com>
2018-02-08 15:23:04 -0800

    New primitives

--- src/hardware_dep/odp/data_plane/odp_primitives.c
+++ src/hardware_dep/odp/data_plane/odp_primitives.c
@@ -1,14 +1,14 @@
 #include "backend.h"
 #include "dataplane.h"
 
-//void add_header(packet_descriptor_t* p, uint8_t hdr_prefix)
 void add_header(packet_descriptor_t* p, header_instance_t hdr_prefix)
 {
     debug("calling add_header \n");
     if(p->headers[hdr_prefix].pointer == NULL) {
         uint16_t len = header_instance_byte_width[hdr_prefix];
-        char* address = odp_packet_push_head(p->pointer, len); // TODO if not to the front?
-        p->headers[hdr_prefix] =
+        uint32_t new = odp_packet_headroom (*((odp_packet_t *)(p->wrapper)));
+        char* address = odp_packet_push_head(*((odp_packet_t *)(p->wrapper)), len);
+       p->headers[hdr_prefix] =
             (header_descriptor_t) {
                 .type = hdr_prefix,
                 .pointer = address,
@@ -19,23 +19,25 @@ void add_header(packet_descriptor_t* p, header_instance_t hdr_prefix)
     }
 }
 
-#if 0
-void remove_header(packet_descriptor_t* p, header_reference_t h)
+void remove_header(packet_descriptor_t* p, header_reference_t hdr_prefix)
 {
-    header_descriptor_t hd = p->headers[h.header_instance];
-    if(hd.pointer != NULL) {
-        uint16_t len = (uint16_t)hd.length;
-        rte_pktmbuf_adj(p->pointer, len);  // if not from the front?
-        p->headers[h.header_instance].length = 0;
-        p->headers[h.header_instance].pointer = NULL;
-    } else {
+    debug("calling remove_header \n");
+    if(p->headers[hdr_prefix].pointer == NULL) {
         printf("Cannot remove a header instance not present in the packet\n");
     }
-}
-#endif
+    else {
+        uint16_t len = header_instance_byte_width[hdr_prefix];
+        char* address =  odp_packet_pull_head(*((odp_packet_t *)(p->wrapper)), len);           p->headers[hdr_prefix] =
+           (header_descriptor_t) {
+                .type = hdr_prefix,
+                .pointer = address,
+                .length = len
+            };
 
-void
-generate_digest(backend bg, char* name, int receiver, struct type_field_list* digest_field_list)
+    }
+}
+
+void generate_digest(backend bg, char* name, int receiver, struct type_field_list* digest_field_list)
 {
     digest d = create_digest(bg, name);
     int i;
